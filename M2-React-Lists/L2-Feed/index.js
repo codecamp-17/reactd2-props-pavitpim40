4b74cf22599551797ebb4ccd4502cfc86c5dc49c
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-26 11:49:26 +0700

    update

--- M2-React-Lists/L2-Feed/index.js
+++ M2-React-Lists/L2-Feed/index.js
@@ -19,3 +19,45 @@
 
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+function Card(props) {
+  // dynamic : title,author,DateTime
+  return (
+    <div class='card'>
+      <h3>{props.title}</h3>
+      <div class='card__footer'>
+        <p>Author : {props.author}</p>
+        <p>{props.date}</p>
+      </div>
+    </div>
+  );
+}
+function App() {
+  const data = [
+    {
+      title: 'Happy new year',
+      author: 'John',
+      date: '2022',
+    },
+    {
+      title: 'Programming is Fun',
+      author: 'Jame',
+      date: '2023',
+    },
+    {
+      title: 'Oh Amazing !!',
+      author: 'Jin',
+      date: '2024',
+    },
+  ];
+
+  return (
+    <div class='container'>
+      {data.map((item) => (
+        <Card title={item.title} author={item.author} date={item.date} />
+      ))}
+    </div>
+  );
+}
+
+root.render(<App />);
